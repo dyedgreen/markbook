eadd5545ee709d6f964fbd616c2b49d7d10eff07
Tilman Roeder <dyed.green.info@gmail.com>
2019-07-01 07:30:52 +0100

    Removed non-shared stuff from parser.h

--- src/notebook/parse.c
+++ src/notebook/parse.c
@@ -1,6 +1,13 @@
 #include <string.h>
+#include "md4c/md4c.h"
 #include "parse.h"
 
+#define PARSER_FLAGS (\
+  MD_FLAG_NOHTML | \
+  MD_FLAG_COLLAPSEWHITESPACE | \
+  MD_FLAG_STRIKETHROUGH | \
+  MD_FLAG_PERMISSIVEAUTOLINKS | \
+  MD_FLAG_LATEX)
 
 
 /* Parse State */
--- src/notebook/parse.h
+++ src/notebook/parse.h
@@ -1,14 +1,5 @@
 #pragma once
 
 #include "sds/sds.h"
-#include "md4c/md4c.h"
-
-
-#define PARSER_FLAGS (\
-  MD_FLAG_NOHTML | \
-  MD_FLAG_COLLAPSEWHITESPACE | \
-  MD_FLAG_STRIKETHROUGH | \
-  MD_FLAG_PERMISSIVEAUTOLINKS | \
-  MD_FLAG_LATEX)
 
 sds parse(const char* text);
